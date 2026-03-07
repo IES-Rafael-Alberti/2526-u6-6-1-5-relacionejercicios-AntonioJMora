a199969e75243985b36c9dd632978cc488a77a81
nmorcab2106 <amorac@iesrafaelalberti.es>
2026-03-07 13:17:18 +0100

    Ejercicio 2 codigo

--- src/main/kotlin/es/ies/ejercicios/u6/ej62/ReportTemplate.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej62/ReportTemplate.kt
@@ -9,11 +9,17 @@ package es.ies.ejercicios.u6.ej62
  */
 abstract class ReportTemplate {
     final fun generate(title: String, lines: List<String>): String =
-        TODO("Implementa el algoritmo común (template method) y usa header/formatLine/footer")
+        buildString {
+            appendLine(header(title))
+            lines.forEach {appendLine (formatLine(it))}
+            appendLine(footer())
+        }
 
-    protected open fun header(title: String): String = TODO("Opcional: cabecera común/por subtipo")
+    protected open fun header(title: String): String =
+        "----- $title -----"
 
     protected abstract fun formatLine(line: String): String
 
-    protected open fun footer(): String = TODO("Opcional: pie común/por subtipo")
+    protected open fun footer(): String =
+        "-------------"
 }
